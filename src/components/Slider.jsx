9a7338de8c7b77fc5c37df7563cc25fbc54971eb
neelgoyani702 <103193259+neelgoyani702@users.noreply.github.com>
2024-07-27 11:05:18 +0530

    Carousel added
    Co-authored-by: Divy Kaila <110351992+Divy2103@users.noreply.github.com>

---
+++ src/components/Slider.jsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+
+import { Card, CardContent } from "./ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+
+const ImageArray = [
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1721655799196-7c50607778a9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Image 1",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1721905126607-6700881aaa95?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Image 2",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1698138819865-88d3add4838f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyOHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Image 3",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1721655799196-7c50607778a9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Image 4",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1721655799196-7c50607778a9?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Image 5",
+  },
+];
+
+export default function Slider() {
+  return (
+    <div className="w-screen ">
+      <Carousel className="relative w-full">
+        <CarouselContent>
+          {ImageArray.map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="flex flex-col items-center justify-center w-full bg-cover bg-center bg-no-repeat h-96 p-5">
+                <img
+                  src={_.imageUrl}
+                  alt={_.alt}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="absolute top-1/2 left-0 transform -translate-y-1/2" />
+        <CarouselNext className="absolute top-1/2 right-0 transform -translate-y-1/2" />
+      </Carousel>
+    </div>
+  );
+}
--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar.jsx'
+import Slider from '../../components/Slider.jsx'
 
 function Home() {
     return (
-        <div className='py-20'>
+        <div className='py-32 md:py-20'>
             <Navbar />
+            <Slider />
             Home
         </div>
     )
